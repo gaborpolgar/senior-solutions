9e84c20885f702e833fd47377adbb53bb5d5cffc
Gábor Polgár <56192725+gaborpolgar@users.noreply.github.com>
2021-07-02 23:50:39 +0200

    Bevezetés a JUnit használatába

--- locations-solution/src/main/java/Location.java
+++ locations-solution/src/main/java/Location.java
@@ -1,5 +1,6 @@
 public class Location {
 
+
     private String name;
     private double lat;
     private double lon;
--- locations-solution/src/main/java/LocationParser.java
+++ locations-solution/src/main/java/LocationParser.java
@@ -1,5 +1,6 @@
 public class LocationParser {
 
+
     public Location parse(String text) {
         String[] parts = text.split(",");
         return new Location(parts[0], Double.parseDouble(parts[1]), Double.parseDouble(parts[2]));
